2e8e77bf11c6485d7a79a0107987d630e152f993
Orilla_Lin <142280255+rillala@users.noreply.github.com>
2024-02-01 13:03:27 +0800

    增加後台登入變數 開發用

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import headerCom from "@/components/header.vue";
+import loginPage from "@/views/LoginPageView.vue";
+
+// 開發後台登入頁面用變數 isLogin, 一般切後台內頁請設置為 "true", 之後接登入 token
+const isLogin = true;
 </script>
 
 <template>
-  <main id="app-wrap">
-    <headerCom id="app-nav" />
-    <div id="app-content">
-      <RouterView id="app-main-view" />
-    </div>
+  <main>
+    <section v-if="!isLogin"><loginPage /></section>
+    <section v-else id="app-wrap">
+      <headerCom id="app-nav" />
+      <div id="app-content">
+        <RouterView id="app-main-view" />
+      </div>
+    </section>
   </main>
 </template>
 
---
+++ src/views/LoginPageView.vue
@@ -0,0 +1,9 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <main>123 show this page</main>
+</template>
+
+<style lang="scss" scoped></style>
