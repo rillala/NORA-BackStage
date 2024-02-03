bf232b145a1db1a3bece3166c60e918967eff93c
Orilla_Lin <142280255+rillala@users.noreply.github.com>
2024-02-03 16:28:02 +0800

    增加跨域設定 api

--- src/views/ReserveView.vue
+++ src/views/ReserveView.vue
@@ -1,5 +1,23 @@
 <script>
-export default {};
+import apiInstance from "@/plugins/auth";
+
+export default {
+  mounted() {
+    this.test();
+  },
+  methods: {
+    test() {
+      apiInstance
+        .get("/test.php")
+        .then((response) => {
+          console.log(response.data); // 这里应该能看到 { "number": 123 }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    },
+  },
+};
 </script>
 
 <template>
