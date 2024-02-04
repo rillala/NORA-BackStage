f16f3b52b5aebc4aa409de6d22d71d76e4294faa
ortis88 <caesar0718@gmail.com>
2024-02-04 11:06:57 +0800

    後台登入ing

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,26 @@
 import { RouterLink, RouterView } from "vue-router";
 import headerCom from "@/components/header.vue";
 import loginPage from "@/views/LoginPageView.vue";
+import { ref } from 'vue';
+// import { mapActions } from 'pinia';
+// import userStore from '@/stores/user';
+
+// const userStore = useStore('userStore');
+
+// // 映射 actions
+// const { updateToken, updateName, checkLogin } = userStore;
+
+// // 在 setup 函數中返回映射後的 actions
+// return {
+//   updateToken,
+//   updateName,
+//   checkLogin,
+// };
+//卡關中...
+
+const isLogin = true;
 
 // 開發後台登入頁面用變數 isLogin, 一般切後台內頁請設置為 "true", 之後接登入 token
-const isLogin = false;
 </script>
 
 <template>
--- src/views/LoginPageView.vue
+++ src/views/LoginPageView.vue
@@ -1,14 +1,45 @@
 <script>
+import axios from 'axios';
+import { mapActions } from 'pinia';
+import userStore from '@/stores/user';
+
 export default {
   data() {
     return {
-      adminName: '',
-      adminPwwd: '',
+      adminName: 'mor_2314',
+      adminPwwd: '83r5^_',
     };
   },
   methods: {
-    siginin() {
-      console.log('login');
+
+    ...mapActions(userStore, ['updateToken']),
+    signin() {
+
+      axios.post('https://fakestoreapi.com/auth/login', {
+        username: this.adminName,
+        password: this.adminPwwd,
+        // }, {
+        //   headers: {
+        //     'Content-Type': 'application/json'
+        //   }
+        //   //fetch一定要加不然抓不到
+      })
+        .then(response => {
+          if (response.data && response.data.token) {
+            // localStorage.setItem('token', response.data.token)
+            // this.updateToken('123')
+            // console.log(login)
+            this.updateToken(response.data.token);
+            console.log('login success', response.data.token);
+          }
+        })
+        .catch(error => {
+          console.error(error)
+          alert('登入失敗');
+          // error時執行這邊
+          // 登入失敗
+          // 系統維護中
+        });
     }
   }
 };
@@ -23,16 +54,14 @@ export default {
           <h4>管理員登入</h4>
         </div>
         <!-- 登入表單 -->
-        <form action="">
-          <label id="adminName">
-            帳號：<input type="text" id="adminName" v-model="adminName"><br />
-          </label>
-          <label id="adminPwwd">
-            密碼：<input type="text" id="adminPwwd" v-model="adminPwwd"><br />
-          </label>
-          <button>登入</button>
-        </form>
-
+        <label id="adminName">
+          帳號：<input type="text" id="adminName" v-model="adminName"><br />
+        </label>
+        <label id="adminPwwd">
+          密碼：<input type="password" id="adminPwwd" v-model="adminPwwd"><br />
+        </label>
+        <button @click="signin">登入</button>
+        <!-- 登入失敗提示 -->
       </div>
     </div>
   </main>
@@ -77,29 +106,14 @@ main {
   }
 }
 
-form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
-  margin: 10px;
-
-  button {
-    font-size: 16px;
-    width: 80px;
-    height: 30px;
-    border: none;
-    border-radius: 10px;
-    margin: 20px;
-  }
-}
-
 #adminName {
   color: $white01;
   font-weight: bold;
+  margin: 5px 0;
 
   input {
     color: #000;
+    font-weight: normal;
     padding: 0 10px;
   }
 }
@@ -107,10 +121,20 @@ form {
 #adminPwwd {
   color: $white01;
   font-weight: bold;
+  margin: 5px 0;
 
   input {
     color: #000;
+    font-weight: normal;
     padding: 0 10px;
   }
+}
+
+button {
+  width: 80px;
+  height: 30px;
+  border: none;
+  border-radius: 10px;
+  margin: 10px;
 }
 </style>
