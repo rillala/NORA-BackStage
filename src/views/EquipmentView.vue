6a187dbc01f0d384a993e25bedfe0180f27e5199
Orilla_Lin <142280255+rillala@users.noreply.github.com>
2024-02-19 17:29:57 +0800

    裝備管理:新增刪除修改功能

--- src/views/EquipmentView.vue
+++ src/views/EquipmentView.vue
@@ -1,37 +1,43 @@
 <script>
 import apiInstance from "@/plugins/auth";
+import { getImageUrl } from "@/assets/js/common";
 
 export default {
   data() {
     return {
       // 搜尋用
       search: "",
-      selectedList: [],
+      displayList: [],
 
       // 編輯用
       editIndex: -1,
-      editInfo: "",
+      // 用於儲存修改或新增的資料
+      EquipData: {
+        id: -1,
+        title: "",
+        info: "",
+        image: "", // 圖片路徑和檔案名稱類型
+        price: 0,
+      },
+      // 用於存儲選擇的圖片檔案
+      newImage: null,
 
       // 表格設定 iView 相關
       columns: [
         {
-          type: "selection",
-          width: "60",
-          align: "center",
-        },
-        {
-          title: "裝備編號",
+          title: "編號",
           key: "id",
           width: "120",
           align: "center",
           sortable: true,
         },
         {
-          title: "裝備名稱",
+          title: "名稱",
           key: "title",
           width: "120",
           align: "center",
           sortable: true,
+          slot: "title",
         },
         {
           title: "租借價格",
@@ -39,12 +45,21 @@ export default {
           width: "120",
           align: "center",
           sortable: true,
+          slot: "price",
         },
         {
-          title: "裝備描述",
+          title: "描述",
           key: "info",
           width: "200",
+          align: "center",
           slot: "info",
+        },
+        {
+          title: "圖片",
+          key: "image",
+          width: "150",
+          align: "center",
+          slot: "image",
         },
         {
           title: "狀態",
@@ -52,6 +67,7 @@ export default {
           width: "100",
           align: "center",
           sortable: true,
+
           slot: "status",
         },
         {
@@ -68,18 +84,18 @@ export default {
         },
       ],
 
+      // 初始讀取值
       equipList: [],
 
-      // 用於儲存修改或新增的資料
-      EquipData: {
-        title: 0,
+      // 新增裝備
+      addBox: false,
+      imagePreview: null,
+      addData: {
+        title: "",
         info: "",
-        image: "", // 圖片路徑和檔案名稱類型
-        status: 1,
+        image: "",
         price: 0,
       },
-      // 用於存儲選擇的圖片檔案
-      selectedFile: null,
     };
   },
   watch: {
@@ -87,6 +103,7 @@ export default {
       let searchList = this.equipList.filter((item) =>
         item.title.toLowerCase().includes(newVal.toLowerCase())
       );
+      this.displayList = searchList;
     },
   },
   created() {
@@ -97,24 +114,52 @@ export default {
     formatPrice(price) {
       return "$" + price.toLocaleString("en-US");
     },
-    // View UI 連動函數
-    remove(index) {
-      this.selectedList.splice(index, 1);
 
-      // this.deleteEquipDb();
-      // this.getPHP();
+    getImageUrl(paths) {
+      return getImageUrl(paths);
     },
-    handleEdit(row, index) {
-      this.editInfo = row.info;
+
+    // View UI 連動函數
+    openEdit(row, index) {
+      // 打開編輯器
       this.editIndex = index;
+
+      // 給予原始的值
+      this.EquipData.id = this.displayList[index].id;
+      this.EquipData.title = this.displayList[index].title;
+      this.EquipData.info = this.displayList[index].info;
+      this.EquipData.price = this.displayList[index].price;
     },
-    handleSave(index) {
-      this.selectedList[index].info = this.editInfo;
+    editSave(index) {
+      // 判斷資料是否有差異
+      this.editEquipToDb();
+
+      // 判斷圖片是否有新值
+      this.changeImage();
       this.editIndex = -1;
     },
     statusChange(index) {
-      this.selectedList[index].open_status =
-        !this.selectedList[index].open_status;
+      this.displayList[index].status = !this.displayList[index].status;
+      let newStatus = this.displayList[index].status == true ? 1 : 0;
+      let currentId = this.displayList[index].id;
+      console.log(newStatus);
+
+      let editItem = new FormData();
+      editItem.append("tablename", "equipment");
+      editItem.append("status", newStatus);
+      editItem.append("id", currentId);
+      console.log(editItem);
+
+      apiInstance
+        .post("editStatus.php", editItem)
+        .then((response) => {
+          if (!response.data.error) {
+            alert(response.data.msg);
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
     },
 
     // PHP 相關 func
@@ -122,9 +167,12 @@ export default {
       apiInstance
         .get("getEquipment.php")
         .then((response) => {
-          console.log(response.data); // 这里应该能看到 { "number": 123 }
+          console.log(response.data);
           this.equipList = response.data.all;
-          this.selectedList = this.equipList;
+          this.displayList = this.equipList.map((item) => ({
+            ...item,
+            status: item.status == 1,
+          }));
         })
         .catch((error) => {
           console.error("Error:", error);
@@ -133,18 +181,16 @@ export default {
 
     // 以下新增用, 還沒串
     addEquipToDb() {
-      if (this.siteData.type_id === "0") {
-        alert("營位種類必選喔!!!");
-        return;
-      }
+      //
+      this.changeImage();
 
       apiInstance
-        .post("campSiteAdd.php", this.siteData)
+        .post("addEquipment.php", this.addData)
         .then((response) => {
           if (!response.data.error) {
             alert(response.data.msg);
-            this.getSitesPHP();
-            this.clearForm(); // 假設您有一個 clearForm 方法來清空表單
+            this.clearAddData();
+            this.getPHP();
           }
         })
         .catch((error) => {
@@ -152,39 +198,104 @@ export default {
         });
     },
 
-    // 新增圖片
-    handleFileSelection(event) {
-      // 存儲選擇的文件
-      this.selectedFile = event.target.files[0];
+    clearAddData() {
+      this.addBox = false;
+      this.addData = {
+        title: "",
+        info: "",
+        image: "",
+        price: 0,
+      };
+      this.imagePreview = null;
     },
-    uploadFile() {
-      if (this.selectedFile) {
-        const formData = new FormData();
-        formData.append("file", this.selectedFile);
 
-        // 使用 apiInstance 進行文件上傳
+    // 更新圖片或增加新圖片
+    handleBeforeUpload(file) {
+      this.newImage = new FormData();
+      this.newImage.append("file", file);
+      let filePath = "equipment/" + file.name;
+
+      if (this.editIndex >= 0) {
+        const oldImgPath = "../image/" + this.displayList[this.editIndex].image;
+        this.newImage.append("oldImgPath", oldImgPath);
+        this.EquipData.image = filePath;
+      } else {
+        // 預覽圖片
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+          this.imagePreview = e.target.result;
+        };
+
+        reader.readAsDataURL(file);
+
+        // 預設圖片路徑
+        this.addData.image = filePath;
+      }
+
+      console.log(filePath);
+
+      // 阻止默認的上傳行為
+      return false;
+    },
+    changeImage() {
+      if (this.newImage) {
         apiInstance
-          .post("upload.php", formData, {
+          .post("addEquipmentImage.php", this.newImage, {
             headers: {
               "Content-Type": "multipart/form-data",
             },
           })
           .then((response) => {
-            console.log("Upload successful:", response.data);
-            // 處理上傳成功的響應
+            // 處理上傳成功的情況
+            console.log("Upload successful", response);
+            alert("已成功更新圖片!");
           })
           .catch((error) => {
-            console.error("Upload error:", error);
             // 處理上傳失敗的情況
+            console.error("Upload error", error);
+            alert("更新失敗圖片!");
           });
       }
     },
 
     // 刪除用
-    deleteEquipDb(id) {},
+    deleteEquipDb(index) {
+      if (confirm("是否確認刪除？")) {
+        let selectItem = this.displayList[index];
+
+        let deleteItem = new FormData();
+        deleteItem.append("tablename", "equipment");
+        deleteItem.append("id", selectItem.id);
+
+        apiInstance
+          .post("deleteData.php", deleteItem)
+          .then((response) => {
+            if (!response.data.error) {
+              alert(response.data.msg);
+              this.getPHP();
+            }
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      }
+    },
 
     // 編輯用
-    editEquipDb(id) {},
+    editEquipToDb() {
+      apiInstance
+        .post("editEquipment.php", this.EquipData)
+        .then((response) => {
+          if (!response.data.error) {
+            alert(response.data.msg);
+            this.getPHP();
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    },
   },
 };
 </script>
@@ -206,10 +317,44 @@ export default {
 
     <br />
 
-    <Table class="table" height="500" :columns="columns" :data="selectedList">
+    <Table class="table" height="500" :columns="columns" :data="displayList">
+      <template #title="{ row, index }">
+        <Input
+          type="text"
+          v-model="EquipData.title"
+          v-if="editIndex == index"
+        />
+        <span v-else>{{ row.title }}</span>
+      </template>
+      <template #price="{ row, index }">
+        <Input
+          type="number"
+          v-model="EquipData.price"
+          v-if="editIndex == index"
+        />
+        <span v-else>{{ row.price }}</span>
+      </template>
+
       <template #info="{ row, index }">
-        <Input type="text" v-model="editInfo" v-if="editIndex === index" />
+        <Input
+          type="text"
+          v-model="EquipData.info"
+          v-if="editIndex === index"
+        />
         <span v-else>{{ row.info }}</span>
+      </template>
+
+      <template #image="{ row, index }">
+        <Upload :before-upload="handleBeforeUpload" v-if="editIndex === index">
+          <Button icon="md-add">更新圖片</Button>
+        </Upload>
+        <Image
+          v-else
+          :src="getImageUrl(row.image)"
+          :alt="row.title"
+          width="50px"
+          height="50px"
+        />
       </template>
 
       <template #status="{ row, index }">
@@ -217,14 +362,14 @@ export default {
         <Switch
           true-color="#13ce66"
           false-color="#ff4949"
-          v-model="row.open_status"
+          v-model="row.status"
           @on-change="statusChange(index)"
         />
       </template>
 
       <template #action="{ row, index }">
         <div v-if="editIndex === index">
-          <Button type="text" @click="handleSave(index)"
+          <Button type="text" @click="editSave(index)"
             ><img src="@/assets/image/icon/save.svg" alt="saveBtn" />
           </Button>
           <Button type="text" @click="editIndex = -1"
@@ -232,18 +377,99 @@ export default {
           </Button>
         </div>
         <div v-else>
-          <Button size="small" type="text" @click="handleEdit(row, index)"
+          <Button size="small" type="text" @click="openEdit(row, index)"
             ><img src="@/assets/image/icon/edit.svg" alt="editBtn" />
           </Button>
         </div>
       </template>
 
       <template #delete="{ row, index }">
-        <Button class="btn" type="text" size="small" @click="remove(index)"
+        <Button
+          class="btn"
+          type="text"
+          size="small"
+          @click="deleteEquipDb(index)"
           ><img src="@/assets/image/icon/delete.svg" alt="deleteBtn" />
         </Button>
       </template>
     </Table>
+
+    <Button class="addBtn" @click="addBox = true">新增商品</Button>
+
+    <Modal
+      title="新增裝備"
+      v-model="addBox"
+      class="vertical-center-modal"
+      width="600"
+      ok-text="確定"
+      cancel-text="取消"
+    >
+      <List item-layout="vertical">
+        <Form>
+          <ListItem justify="center" align="middle">
+            <Image
+              v-if="imagePreview"
+              :src="imagePreview"
+              alt="Image preview"
+              width="200px"
+              height="200px"
+            />
+            <Upload :before-upload="handleBeforeUpload">
+              <Button icon="md-add">上傳裝備照片</Button>
+            </Upload>
+          </ListItem>
+
+          <ListItem>
+            <Row class="form-row" justify="center" align="middle">
+              <Col span="5" align="center" class="row-title">
+                <span>裝備名稱：</span>
+              </Col>
+              <Col span="19">
+                <Input v-model="addData.title" placeholder="請輸入名稱" />
+              </Col>
+            </Row>
+          </ListItem>
+
+          <ListItem justify="center" align="middle">
+            <Row class="form-row" justify="center" align="middle">
+              <Col span="5" align="center" class="row-title">
+                <span>裝備說明：</span>
+              </Col>
+              <Col span="19">
+                <Input
+                  type="textarea"
+                  :rows="5"
+                  v-model="addData.info"
+                  maxlength="200"
+                  show-word-limit
+                  placeholder="請輸入說明"
+                />
+              </Col>
+            </Row>
+          </ListItem>
+
+          <ListItem>
+            <Row class="form-row" justify="center" align="middle">
+              <Col span="5" align="center">
+                <span>裝備價格：</span>
+              </Col>
+              <Col span="17">
+                <FromeItem>
+                  <Input v-model="addData.price" placeholder="請輸入單價" />
+                </FromeItem>
+              </Col>
+              <Col span="2" align="center">
+                <span>元</span>
+              </Col>
+            </Row>
+          </ListItem>
+        </Form>
+      </List>
+      <template #footer>
+        <Button type="dashed" @click="clearAddData">取消</Button>
+        <Button type="primary" @click="addEquipToDb">儲存</Button>
+      </template>
+    </Modal>
   </main>
 </template>
 
@@ -275,5 +501,25 @@ h4 {
 
 .table {
   width: 100%;
+}
+
+.addBtn {
+  display: block;
+  margin: 10px 0 0 auto;
+}
+
+//新增商品
+.ivu-row {
+  display: flex;
+  flex-flow: row nowrap;
+}
+.form-row {
+  margin: 5px 40px;
+}
+
+.dynamic-row {
+  width: 100%;
+  padding: 5px 0px 5px 55px;
+  margin-bottom: 0px;
 }
 </style>
