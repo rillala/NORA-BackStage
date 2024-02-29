f4ef2372b6fcaa4920b5221b24470152df75890a
Orilla_Lin <142280255+rillala@users.noreply.github.com>
2024-02-29 09:48:36 +0800

    fix 型別轉換

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -20,57 +20,55 @@ export default {
       //表格欄位、屬性屬性值
       columns: [
         {
-          title: '管理員編號',
-          key: 'adminid',
-          align: 'center',
-          ellipsis: 'true',
-          width: '150'
+          title: "管理員編號",
+          key: "adminid",
+          align: "center",
+          ellipsis: "true",
+          width: "150",
         },
         {
-          title: '管理員名稱',
-          key: 'name',
-          align: 'center',
-          ellipsis: 'true',
-          width: '150',
-          slot: 'name',
+          title: "管理員名稱",
+          key: "name",
+          align: "center",
+          ellipsis: "true",
+          width: "150",
+          slot: "name",
         },
         {
-          title: '管理員帳號',
-          key: 'acc',
-          align: 'center',
-          width: '220',
-          slot: 'acc',
-
+          title: "管理員帳號",
+          key: "acc",
+          align: "center",
+          width: "220",
+          slot: "acc",
         },
         {
-          title: '管理員密碼',
-          key: 'psw',
-          align: 'center',
-          width: '220',
-          slot: 'psw',
-
+          title: "管理員密碼",
+          key: "psw",
+          align: "center",
+          width: "220",
+          slot: "psw",
         },
         {
-          title: '帳號狀態',
-          key: 'status',
-          width: '100',
-          align: 'center',
-          slot: 'status',
+          title: "帳號狀態",
+          key: "status",
+          width: "100",
+          align: "center",
+          slot: "status",
         },
         {
-          title: '編輯',
-          align: 'center',
-          width: '100',
-          slot: 'edit'
-        }
+          title: "編輯",
+          align: "center",
+          width: "100",
+          slot: "edit",
+        },
       ],
       //新增燈箱開啟關閉
       modalAdd: false,
       //新增管理員輸入
       addAdminData: {
-        name: '',
-        acc: '',
-        psw: '',
+        name: "",
+        acc: "",
+        psw: "",
         status: 1,
       },
       //新增管理員第一次密碼輸入
@@ -81,22 +79,21 @@ export default {
     this.getAdminPHP();
   },
   methods: {
-
-
     //讀取admin資料庫
     getAdminPHP() {
       apiInstance
-        .get("./getAdmin.php")
+        .get("getadmin.php")
         .then((response) => {
           //將回傳管理員狀態從1或0轉換成布林值
           const adminList = response.data.all.map((admin) => ({
             ...admin,
-            status: admin.status === 1 ? true : false
+            status: admin.status == 1 ? true : false,
           }));
           //讀取進data
           this.adminList = adminList;
           console.log(this.adminList);
-        }).catch((error) => {
+        })
+        .catch((error) => {
           console.error("Error", error);
         });
     },
@@ -107,23 +104,28 @@ export default {
         this.addAdmin();
       } else {
         // 密碼不一致，彈出提示
-        alert('兩次輸入的密碼不一致');
+        alert("兩次輸入的密碼不一致");
       }
     },
 
     clearForm() {
-      this.pswfirst = "",
-        this.addAdminData = {
-          name: '',
-          acc: '',
-          psw: '',
-        };
+      (this.pswfirst = ""),
+        (this.addAdminData = {
+          name: "",
+          acc: "",
+          psw: "",
+        });
     },
 
     addAdmin() {
-
-      if (!(this.addAdminData.name && this.addAdminData.acc && this.addAdminData.psw)) {
-        alert('請填寫所有輸入值');
+      if (
+        !(
+          this.addAdminData.name &&
+          this.addAdminData.acc &&
+          this.addAdminData.psw
+        )
+      ) {
+        alert("請填寫所有輸入值");
         return;
       }
 
@@ -142,11 +144,11 @@ export default {
           } else {
             alert(response.data.msg);
           }
-        }).catch((error) => {
+        })
+        .catch((error) => {
           console.error("Error", error);
         });
     },
-
 
     //打開編輯器
     openEdit(row, index) {
@@ -159,7 +161,7 @@ export default {
     },
     //儲存更新動作
     saveEdit(index) {
-      this.updatEdit()
+      this.updatEdit();
     },
     //更新資料庫
     updatEdit() {
@@ -174,7 +176,8 @@ export default {
           } else {
             alert(response.data.msg);
           }
-        }).catch((error) => {
+        })
+        .catch((error) => {
           console.error("Error", error);
         });
     },
@@ -209,32 +212,43 @@ export default {
 
     //編輯自身狀態判別
     identifySelf(index) {
-      const loginId = Number(localStorage.getItem('adminId'));
-      const result = this.adminList[index].adminid !== loginId;
-      return result;
+      // const loginId = Number(localStorage.getItem("adminId"));
+      // const result = this.adminList[index].adminid !== loginId;
+      // return result;
+      return this.adminList[index].adminid ==
+        parseInt(localStorage.getItem("adminId"))
+        ? false
+        : true;
     },
   },
-}
+};
 </script>
 
 <template>
   <!-- 新增燈箱 -->
   <Modal title="新增管理員" v-model="modalAdd">
-    <div style="display: flex; flex-direction:column ; align-items: center;">
-      <Space class="addInput">新增名稱：<Input v-model="addAdminData.name" />
+    <div style="display: flex; flex-direction: column; align-items: center">
+      <Space class="addInput"
+        >新增名稱：<Input v-model="addAdminData.name" />
       </Space>
-      <Space class="addInput">新增帳號：<Input v-model="addAdminData.acc" />
+      <Space class="addInput"
+        >新增帳號：<Input v-model="addAdminData.acc" />
       </Space>
-      <Space class="addInput">新增密碼：<Input type="password" v-model="pswfirst" />
+      <Space class="addInput"
+        >新增密碼：<Input type="password" v-model="pswfirst" />
       </Space>
-      <Space style="position: relative; right: 14px;" class="addInput">再次輸入密碼：<Input type="password"
-          v-model="addAdminData.psw" />
+      <Space style="position: relative; right: 14px" class="addInput"
+        >再次輸入密碼：<Input type="password" v-model="addAdminData.psw" />
       </Space>
     </div>
     <template #footer>
       <Button @click="modalAdd = false">取消</Button>
-      <Button type="primary" @click="pswIdentify"
-        :disabled="!addAdminData.name || !addAdminData.acc || !addAdminData.psw">送出</Button>
+      <Button
+        type="primary"
+        @click="pswIdentify"
+        :disabled="!addAdminData.name || !addAdminData.acc || !addAdminData.psw"
+        >送出</Button
+      >
     </template>
   </Modal>
 
@@ -246,50 +260,73 @@ export default {
       <!-- <Input class="search-input" search enter-button placeholder="請輸入管理員編號進行搜尋" v-model="search" /> -->
     </div>
 
-    <Space type="flex" style="justify-content: start; padding: 10px;">
+    <Space type="flex" style="justify-content: start; padding: 10px">
       <Button type="primary" @click="modalAdd = true">新增管理員</Button>
     </Space>
 
-    <Table class="admin-table" :columns="columns" :data="adminList" height="500">
-
+    <Table
+      class="admin-table"
+      :columns="columns"
+      :data="adminList"
+      height="500"
+    >
       <!--狀態切換開關-->
       <template #status="{ row, index }">
-        <Switch v-if="identifySelf(index)" v-model="row.status" true-color="#13ce66" false-color="#ff4949"
-          @on-change="statusChange(index)" />
+        <Switch
+          v-if="identifySelf(index)"
+          v-model="row.status"
+          true-color="#13ce66"
+          false-color="#ff4949"
+          @on-change="statusChange(index)"
+        />
       </template>
 
       <!-- 開啟編輯欄位 -->
       <template #name="{ row, index }">
-        <Input type="text" size="small" v-model="editAdmin.name" v-if="editIndex == index" />
+        <Input
+          type="text"
+          size="small"
+          v-model="editAdmin.name"
+          v-if="editIndex == index"
+        />
         <span v-else>{{ row.name }}</span>
       </template>
       <template #acc="{ row, index }">
-        <Input type="text" size="small" v-model="editAdmin.acc" v-if="editIndex == index" />
+        <Input
+          type="text"
+          size="small"
+          v-model="editAdmin.acc"
+          v-if="editIndex == index"
+        />
         <span v-else>{{ row.acc }}</span>
       </template>
       <template #psw="{ row, index }">
-        <Input type="text" size="small" v-model="editAdmin.psw" v-if="editIndex == index" />
+        <Input
+          type="text"
+          size="small"
+          v-model="editAdmin.psw"
+          v-if="editIndex == index"
+        />
         <span v-else>{{ row.psw }}</span>
       </template>
-
 
       <!-- 編輯按鈕 -->
       <template #edit="{ row, index }">
         <div v-if="editIndex === index">
-          <Button @click="saveEdit"><img src="@/assets/image/icon/save.svg" alt="saveBtn" /></Button>
+          <Button @click="saveEdit"
+            ><img src="@/assets/image/icon/save.svg" alt="saveBtn"
+          /></Button>
           <Button @click="editIndex = -1">
-            <img src="@/assets/image/icon/close.svg" alt="closeBtn" /></Button>
+            <img src="@/assets/image/icon/close.svg" alt="closeBtn"
+          /></Button>
         </div>
         <div v-else>
-          <Button size="small" type="text" @click="openEdit(row, index)"><img src="@/assets/image/icon/edit.svg"
-              alt="editBtn" />
+          <Button size="small" type="text" @click="openEdit(row, index)"
+            ><img src="@/assets/image/icon/edit.svg" alt="editBtn" />
           </Button>
         </div>
       </template>
-
     </Table>
-
-
   </main>
 </template>
 
@@ -325,7 +362,6 @@ h4 {
 .table {
   width: 100%;
 }
-
 
 .vertical-center-modal {
   display: flex;
